8e77cef332f37aaafd8bccb533abd5a47c51a2e8
bigkittie75 <bigkittie75@gmail.com>
2017-07-12 22:47:18 -0400

    Updating Index.html

--- footer.php
+++
@@ -1,9 +0,0 @@
-      </div>
-      <footer>
-        <span>
-        <a href="mailto:ahaas16@kent.edu">[CONTACT]</a>
-      </span>
-      </footer>
-    </div>
-  </body>
-</html>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include('header.php'); ?>
-
-<h1>Hello World</h1>
-<p>I'm hosted with GitHub Pages.</p>
-
-<?php include('footer.php'); ?>
