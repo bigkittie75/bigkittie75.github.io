f77bc08321c86627b9df67111fb2ee11a6b9bf80
bigkittie75 <bigkittie75@gmail.com>
2017-07-12 22:39:02 -0400

    Updates to index
    added header and footer php files

---
+++ footer.php
@@ -0,0 +1,9 @@
+      </div>
+      <footer>
+        <span>
+        <a href="mailto:ahaas16@kent.edu">[CONTACT]</a>
+      </span>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include('header.php'); ?>
+
+<h1>Hello World</h1>
+<p>I'm hosted with GitHub Pages.</p>
+
+<?php include('footer.php'); ?>
